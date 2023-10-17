107bccd606acd9cd1a29ddd1a05a24c3fc52dbef
Leonardo <leonardo38361@gmail.com>
2023-10-17 00:36:38 -0300

    manejo de response get

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.send('Home page');
+});
+
+app.get('/hola-mundo', (req, res) => {
+	res.send('Hola mundo en su respectiva ruta');
+});
+
+app.get('*', (req, res) => {
+	res.send('404 | Page not found');
+});
+
+app.listen(8080);
