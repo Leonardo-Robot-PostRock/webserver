80bf43a7655dd51fc5142db8a87d2ca8717d7e6d
Leonardo <leonardo38361@gmail.com>
2023-11-01 01:23:39 -0300

    Servir archivos estáticos en express

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Home page');
-});
+//Middlewares
+//Servir contenido estático
+app.use(express.static('public'));
 
 app.get('/hola-mundo', (req, res) => {
 	res.send('Hola mundo en su respectiva ruta');
 });
 
 app.get('*', (req, res) => {
-	res.send('404 | Page not found');
+	res.sendFile(__dirname + '/public/404.html');
 });
 
 app.listen(8080);
